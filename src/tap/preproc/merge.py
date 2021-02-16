e8d523d1267a5f22a94f1af4fb5f85ce0064cc9e
Louis Maddox <lmmx@users.noreply.github.com>
2021-02-16 23:47:47 +0000

    Port merge routine to Python

--- src/tap/preproc/merge.py
+++ src/tap/preproc/merge.py
@@ -6,21 +6,12 @@ __all__ = []
 def gather_m4s_to_mp4(dash_file, m4s_files, output_mp4):
     """
     Concatenate `.dash` and `.m4s` files using the system `cat` facility.
-    Files can be passed either as explicit list of filenames or globbed with
-    a Kleene-star string (note: Python `glob.glob` is unsorted, do not use).
     """
     if output_mp4.exists():
         raise ValueError(f"{output_mp4=} already exists: risk of doubled output")
-    cmd_list = [
-        *"for x in".split(),
-        f'{dash_file}',
-        f'{m4s_files}',
-        *"; do cat $x >>".split(),
-        f'{output_mp4}',
-        *"; done".split(),
-    ]
-    subprocess.call(cmd_list)
-    # TODO: rewrite as glob, this doesn't work
+    for mpeg in [dash_file, *m4s_files]:
+        cmd_list = ["cat", f"{mpeg}", ">>", f"{output_mp4}"]
+        subprocess.call(cmd_list)
 
 def gather_pulled_downloads(input_dir, output_dir):
     """
@@ -32,7 +23,10 @@ def gather_pulled_downloads(input_dir, output_dir):
         raise ValueError(f"No dash file found in {input_dir}")
     elif len(dash_glob) > 1:
         raise ValueError(f"Multiple dash files found in {input_dir}")
+    else:
+        dash_file = dash_glob[0]
     m4s_globstr = f"{input_dir.absolute() / '*.m4s'}"
+    m4s_files = sorted(glob(m4s_globstr))
     output_mp4 = output_dir.absolute() / "output.mp4"
-    gather_m4s_to_mp4(dash_globstr, m4s_globstr, output_mp4)
+    gather_m4s_to_mp4(dash_file, m4s_files, output_mp4)
     return output_mp4
