f57410f215d702c1107ea43b49d7af04e03b09cb
Louis Maddox <louismmx@gmail.com>
2021-03-18 19:53:56 +0000

    Fully chunk summaries; remove requests dependency

--- src/tap/precis/summarise.py
+++ src/tap/precis/summarise.py
@@ -56,9 +56,41 @@ def summarise_in_chunks(doc_list, tok_limit=1024, length_penalty=.5, n_beams=2):
             tokenised = []
             freshly_tokenised = []
     if len(tokenised): # Finish off any leftovers
+        #print(working_text)
         chunk_sizes.append(len(working_text))
         toks_chunked.append(tokenised)
-    assert sum(chunk_sizes) == n_docs, "Did not correctly chunk the input list"
+    if sum(chunk_sizes) != n_docs:
+        # Repeat the while loop process one final time (more than likely will give
+        # a bad summary due to being too short...)
+        #
+        # The working text must be tokenised to finish off
+        to_tokenise = preamble + doc_sep.join(working_text)
+        # Temporarily silence warnings: deliberately surpassing token max. length limit
+        logger = logging.getLogger("transformers.tokenization_utils_base")
+        lvl = logger.level # store original logger level
+        logger.setLevel(logging.ERROR)
+        freshly_tokenised = tokeniser(to_tokenise, return_tensors="pt").to(device)
+        logger.setLevel(lvl) # return to original level
+        if len(freshly_tokenised[0]) < tok_limit:
+            tokenised = freshly_tokenised
+        else:
+            raise ValueError("Could not finish chunking the input list in one pass")
+        #else:
+        #    # Store the number of documents which gave the tokens in the previous chunk
+        #    chunk_sizes.append(len(working_text) - 1)
+        #    # Create a new working text from the document that pushed it over the limit
+        #    working_text = working_text[-1:]
+        #    # Store the last approved tokenisation that was within the limit
+        #    #print(f"Storing a chunk of {len(tokenised[0])}")
+        #    toks_chunked.append(tokenised)
+        #    tokenised = []
+        #    freshly_tokenised = []
+        if len(tokenised): # Finish off any leftovers
+            chunk_sizes.append(len(working_text))
+            toks_chunked.append(tokenised)
+        if sum(chunk_sizes) != n_docs:
+            breakpoint()
+            raise ValueError("Did not correctly chunk the input list")
     summaries = []
     for toks in tqdm(toks_chunked):
         model_outputs = model.to(device).generate(
